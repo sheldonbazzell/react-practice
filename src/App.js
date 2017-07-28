7732bfbb9fa6eb05f7730f3ff01678892913a417
Sheldon Bazzell <sheldon.bazzell@gmail.com>
2017-07-27 21:02:32 -0700

    refactored as stateless functional components

--- src/App.js
+++ src/App.js
@@ -1,67 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      rows: null,
-      colorA: 'red',
-      colorB: 'black'
-    }
-  }
-  componentWillMount() {
-    let rows = prompt('How many rows?');
-    this.setState(() => ({rows:rows}))
-    let colorA = prompt('First color? Note, must be hexadecimal');
-    let colorB = prompt('Second color? Note, must be hexadecimal');
-    if (/^#[0-9a-f]{3}([0-9a-f]{3})?$/i.test(colorA)) {
-        this.setState(() => ({colorA:colorA}))
-    }
-    if (/^#[0-9a-f]{3}([0-9a-f]{3})?$/i.test(colorB)) {
-        this.setState(() => ({colorB:colorB}))
-    }
-  }
-  render() {
-    let width = Math.round(500 / Number(this.state.rows)) + "px";
-    let tmp = Array(Number(this.state.rows)).fill(1)
-    let list = tmp.map(
-      (a, b) => {
-        return tmp.map(
-          (x, y) => {
-            return b %2 === 0 ? y %2 === 0 ? 0 : 1 :
-             b%2 !== 0 ? y %2 === 0 ? 1 : 0 : y %2 === 1 ? 0 : 1
+function Cell(props) {
+  return (
+    <div
+      style={
+          {
+            background:props.background,
+            height:props.height,
+            width:props.width,
+            display:props.display,
+            backgroundColor:props.background
           }
-        )
-      }
-    )
-    let colorA = this.state.colorA;
-    let colorB = this.state.colorB;
-    if (list && width && colorA && colorB) {
-      return (
-        <div id="app">
-          {list && list.map((x,y) => {
-            return (
-              <div key={y} className="row">
-              {list && list[y].map((a,b) => {
-                return (
-                  <div
-                    key={b}
-                    className="cell"
-                    style={a === 0 ? 
-                      {background:colorA, width:'45px', height:'45px', display:'inline-block'} :
-                      {background:colorB, width:'45px', height:'45px', display:'inline-block'}}
-                    >
-                  </div>
-                )
-              })}
-              </div>
-            )
-          })}
-        </div>
-      );
-    }  
-  }
+        }
+    ></div>
+  )
 }
+
+function Row(props) {
+  let cells = [];
+  for (let i = 0; i < props.rows; ++i) {
+    cells.push(<Cell
+      key={i}
+      height={props.css.cell.height}
+      width={props.css.cell.width}
+      display={props.css.cell.display}
+      background={
+        props.zero && i % 2 === 0 ?
+          props.css.colorA :
+        props.zero && i % 2 === 1 ?
+          props.css.colorB :
+        !props.zero && i % 2 === 0 ?
+          props.css.colorB :
+        !props.zero && i % 2 === 1 ?
+          props.css.colorA :
+          props.css.colorB
+      }
+    />)
+  }
+  return (
+    <div
+      style={{width:'100%',height: props.css.row.height}}
+    >
+      {cells}
+    </div>
+  )
+}
+
+function App(props) {
+  let rows = [];
+  if (props.colors[0]) props.styles.colorA = props.colors[0];
+  if (props.colors[1]) props.styles.colorB = props.colors[1];
+  for (let i = 0; i < props.rows; ++i) {
+    rows.push(<Row key={i} zero={i%2 === 0} rows={props.rows} css={props.styles} />)
+  }
+  return (
+    <div>{rows}</div>
+  )
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let styles = {
+  row: {height: '20px'},
+  cell: {height: '20px', width: '20px', display:'inline-block'},
+  colorA: 'black',
+  colorB: 'red'
+}
+
+let rows = prompt("How many rows?");
+let colorA = prompt("First color? Must be hex");
+let colorB = prompt("Second color? Must be hex");
+if (!/^#[0-9a-f]{3}([0-9a-f]{3})?$/i.test(colorA)) {
+    colorA = null;
+}
+if (!/^#[0-9a-f]{3}([0-9a-f]{3})?$/i.test(colorB)) {
+    colorB = null;
+}
+ReactDOM.render(<App styles={styles} colors={[colorA, colorB]} rows={rows} />, document.getElementById('root'));
 registerServiceWorker();
